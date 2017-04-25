a2c9ab62c1a5acb075db9099049947c4aec28685
Mike Ghaussy <mikeghaussy@airbears2-10-142-174-79.airbears2.1918.berkeley.edu>
2017-04-24 17:54:26 -0700

    added rudimentary tuner screen

--- app/src/main/java/com/example/evie/music_assistant/Tuner.java
+++ app/src/main/java/com/example/evie/music_assistant/Tuner.java
@@ -15,6 +15,12 @@ public class Tuner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tunerscreen);
+        Spinner spinner = (Spinner) findViewById(R.id.notes_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.notes_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
+
 
 }

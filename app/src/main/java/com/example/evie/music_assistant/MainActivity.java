c4287848b998519ad34a9eb4659f9bab9c93024a
Mike Ghaussy <mikeghaussy@airbears2-10-142-174-79.airbears2.1918.berkeley.edu>
2017-04-24 17:01:13 -0700

    added other screens and buttons successfully navigate

--- app/src/main/java/com/example/evie/music_assistant/MainActivity.java
+++ app/src/main/java/com/example/evie/music_assistant/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.evie.music_assistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.content.*;
+import android.widget.*;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToTuner(View view) {
+        Intent tunerIntent = new Intent(this, Tuner.class);
+        startActivity(tunerIntent);
+    }
+
+    public void goToMetronome(View view) {
+        Intent metroIntent = new Intent(this, Metronome.class);
+        startActivity(metroIntent);
+    }
+
+    public void goToRecord(View view) {
+        Intent recordIntent = new Intent(this, Metronome.class);
+        startActivity(recordIntent);
     }
 }
---
+++ app/src/main/java/com/example/evie/music_assistant/Record.java
@@ -0,0 +1,20 @@
+package com.example.evie.music_assistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.*;
+import android.content.*;
+import android.widget.*;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Record extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recordscreen);
+    }
+
+}

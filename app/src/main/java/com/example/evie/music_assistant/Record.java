352a070a12eb4becbefc10d9d2035be4ed8355cc
eveyhuang <jiaxinh.evey@gmail.com>
2017-04-26 14:43:16 -0700

    startRecording seems to work now. when click stop button does not work.

--- app/src/main/java/com/example/evie/music_assistant/Record.java
+++ app/src/main/java/com/example/evie/music_assistant/Record.java
@@ -1,7 +1,12 @@
 package com.example.evie.music_assistant;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.*;
 import android.content.*;
 import android.widget.*;
@@ -9,7 +14,19 @@ import android.widget.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Record extends AppCompatActivity {
+    private static final int RECORDER_SAMPLERATE = 8000;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord recorder = null;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
+    private int bufferSizeInBytes;
+    final int SAMPLE_RATE = 44100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +34,122 @@ public class Record extends AppCompatActivity {
         setTitle("Record");
         setContentView(R.layout.recordscreen);
     }
+
+    private void goToRecordList(View view) {
+
+    }
+
+
+
+    int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+    int BytesPerElement = 2; // 2 bytes in 16bit format
+
+    public void startRecording(View view){
+
+       // AudioRecord recorder = findAudioRecord();
+        int bufferSize = AudioRecord.getMinBufferSize(44100,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+        final AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
+                44100,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                bufferSize);
+
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            public void run() {
+                String filePath = Environment.getExternalStorageDirectory().getPath();
+                short sData[] = new short[BufferElements2Rec];
+
+                FileOutputStream os = null;
+                try {
+                    os = new FileOutputStream(filePath+"/record.pcm");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                while (isRecording) {
+                    // gets the voice output from microphone to byte format
+                    recorder.read(sData, 0, BufferElements2Rec);
+                    System.out.println("Short wirting to file" + sData.toString());
+                    try {
+                        // // writes the data to file from buffer
+                        // // stores the voice buffer
+                        byte bData[] = short2byte(sData);
+                        os.write(bData, 0, BufferElements2Rec * BytesPerElement);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+
+    }
+
+
+    //convert short to byte
+    public byte[] short2byte(short[] sData) {
+        int shortArrsize = sData.length;
+        byte[] bytes = new byte[shortArrsize * 2];
+        for (int i = 0; i < shortArrsize; i++) {
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+            sData[i] = 0;
+        }
+        return bytes;
+    }
+
+    public void stopRecording(View view) {
+        // stops the recording activity
+        if (null != recorder) {
+            isRecording = false;
+
+
+            recorder.stop();
+            recorder.release();
+
+            recorder = null;
+            recordingThread = null;
+        }
+    }
+
+    private static int[] mSampleRates = new int[] { 8000, 11025, 22050, 44100 };
+    public AudioRecord findAudioRecord() {
+        for (int rate : mSampleRates) {
+            for (short audioFormat : new short[] { AudioFormat.ENCODING_PCM_16BIT, AudioFormat.ENCODING_PCM_8BIT }) {
+                for (short channelConfig : new short[] { AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO }) {
+                    try {
+                        Log.d("TEST", "Attempting rate " + rate + "Hz, bits: " + audioFormat + ", channel: "
+                                + channelConfig);
+                        int bufferSize = AudioRecord.getMinBufferSize(rate, channelConfig, audioFormat);
+
+                        if (bufferSize != AudioRecord.ERROR_BAD_VALUE) {
+                            // check if we can instantiate and have a success
+                            AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, rate, channelConfig, audioFormat, bufferSize);
+
+                            if (recorder.getState() == AudioRecord.STATE_INITIALIZED)
+                                bufferSizeInBytes=bufferSize;
+                                return recorder;
+                        }
+                    } catch (Exception e) {
+                        Log.e("TEST", rate + "Exception, keep trying.",e);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
 
 }
